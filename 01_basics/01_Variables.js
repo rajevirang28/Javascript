d96d4ce59e404aefe2cc0622e681f747532a22fc
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2024-09-21 16:55:34 +0530

    nothing

--- 01_basics/01_Variables.js
+++
@@ -1,7 +0,0 @@
-const accountID = 123456789                  //const we cannot change data
-let accountEmail = "virangraje28@gmail.com"  //let we can change
-var accountPassword = "12345"                //var we can change and not to use because of scope.
-accountCity = "Pune"                         //not to use
-let accountSate
-
-console.table([accountID, accountEmail, accountPassword, accountCity, accountSate]);
