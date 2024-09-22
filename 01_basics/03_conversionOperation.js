499e676354a87e6be8a7bc8bb1473d5845ddf260
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2024-09-22 05:06:39 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "18abc"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "18" => 18
+// "18abc" => NaN (Not a Number) type is number
+// true => 1; false => 0;
+ 
+let  isLoggedIn = 1
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn)
+
+// 1 => true; 0 => false;
+// "" => false
+// "Virang" => true
+
+let someNumber = 18
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+// 18 => 18 but type is string
