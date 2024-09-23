d3cd1adc5611826c65290ef00ecb3e44e7494446
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2024-09-23 12:45:00 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,38 @@ console.log(stringNumber)
 console.log(typeof stringNumber)
 
 // 18 => 18 but type is string
+
+// Operations
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Virang"
+let str3 = str1 + str2
+console.log(str3)
+
+// not write way to write this below equation.
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+let a = 3
+++a;
+ console.log(a)
+
+ let b = 3
+b++;
+ console.log(a)
