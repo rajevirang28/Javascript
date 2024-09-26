6f577c23cc6a85e3bcd2455a39de050fc4b51e36
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2024-09-26 13:52:35 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "Virang"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value"); old syntax
+
+consloe.log('Hello my name is ${name} and my repo count is ${repoCount}');
+
+const gameName = new String('Virang')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+// Types of Strings
+
