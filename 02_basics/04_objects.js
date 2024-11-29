0f8451aa779addb32677454e7dc9875d51c52e97
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2024-11-29 05:22:10 +0000

    singlenton

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,34 @@
+//const tinderUser = new Objects() // singleton
+const tinderUser = {} //non singleton
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isloggedIn = false
+
+console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname : {
+        userfullname: {
+            firstname: "Virang",
+            lastname: "Raje"
+        }
+    }
+}
+console.log(regularUser.fullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
