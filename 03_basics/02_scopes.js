1b3f5186a13d6f567775e447c263101ee042f44f
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2025-02-10 11:19:41 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,11 +3,11 @@ let a = 200
 if (true){
     let a = 20
     const b = 30
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 
 
@@ -15,8 +15,31 @@ function one(){
     const username = "Virang"
     function two(){
         const website = "youtube"
-        console.log(username);
+        // console.log(username);
     }
     // console.log(website);
     two()
 }
+one()
+
+if (true){
+    const username = "Virang"
+    if(username == "Virang"){
+        const website = "youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// *************** interesting *****************
+
+function addone(num){
+    return num + 1
+}
+addone(5)
+
+const addtwo = function(num){
+    return num + 2
+}
+addtwo(5) 
